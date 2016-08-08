d36a91a8e783ed43564fc6f49a3438ddaf629352
LittleStupid <lttlstpd@live.com>
2016-08-08 13:28:36 +0800

    commment mail

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,12 +8,13 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
 
     if @contact.save
+=begin
       name = params[:contact][:name]
       email = params[:contact][:email]
       body = parans[:contact][:comments]
 
       ContactMailer.contact_email( name, email, body ).deliver
-
+=end
       flash[:success] = "Message sent."
       redirect_to new_contact_path
     else
--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,15 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+=begin
+ActionMailer::Base.smtp_settings = {
+  address: "smtp.qq.com",
+  port: 465,
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: "",
+  password: "",
+  tls: true,
+  ssl: true
+}
+=end
